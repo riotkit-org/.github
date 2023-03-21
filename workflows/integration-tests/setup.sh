55d7656224db08e4b1ffa01568643f8307465b94
B&R <example@example.org>
2023-03-21 14:18:51 +0100

    feat: Added initially integration-tests pipeline

---
+++ workflows/integration-tests/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set +x
+
+SELF_VERSION=${1:-release\-1}
+
+echo " >> Downloading env.mk"
+curl -Ls https://raw.githubusercontent.com/riotkit-org/.github/${SELF_VERSION}/workflows/integration-tests/workspace/env.mk --output env.mk
+
+echo " >> Downloading .env (if not exists)"
+if [[ ! -f .env ]]; then
+    curl -Ls https://raw.githubusercontent.com/riotkit-org/.github/${SELF_VERSION}/workflows/integration-tests/workspace/.env --output .env
+fi
+
+echo " >> Configuring git ignore"
+if [[ ! -f .gitignore ]]; then
+    echo "" > .gitignore
+fi
+if [[ $(cat .gitignore) != *".build/"* ]]; then
+    echo "/.build/*" > .gitignore
+fi
+
+echo " >> Adding files to git and pushing changes"
+git add .env env.mk .gitignore
+git commit -m "chore: Configure environment setup (automatic) / Riotkit"
+git push
