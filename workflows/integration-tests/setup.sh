89e6b1f91aa9b8c4a48f3a839a1a9a061dd36de5
B&R <example@example.org>
2023-03-21 14:25:43 +0100

    feat: Add example skaffold.yaml

--- workflows/integration-tests/setup.sh
+++ workflows/integration-tests/setup.sh
@@ -20,6 +20,11 @@ if [[ $(cat .gitignore) != *".build/"* ]]; then
     echo "/.build/*" > .gitignore
 fi
 
+echo " >> Configuring Skaffold"
+if [[ ! -f skaffold.yaml ]]; then
+    curl -Ls https://raw.githubusercontent.com/riotkit-org/.github/${SELF_VERSION}/workflows/integration-tests/workspace/skaffold.yaml --output skaffold.yaml
+fi
+
 echo " >> Adding files to git and pushing changes"
 git add .env env.mk .gitignore
 git commit -m "chore: Configure environment setup (automatic) / Riotkit"
